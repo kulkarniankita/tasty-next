e1fb3587f4971c9a7d32d099395a8ee541d39f78
Ankita Kulkarni <kulkarni.ankita09@gmail.com>
2024-03-22 12:49:54 -0400

    shadcn + navbar + base

---
+++ app/recipes/[recipeId]/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Page() {
+  return <div>Hi! I am a recipe page</div>;
+}
---
+++ components/ui/nav-bar.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import React from "react";
+
+export default function NavBar() {
+  return (
+    <nav className="w-full border-0 py-4 lg:px-24 px-10 bg-gray-950">
+      <h1 className="text-3xl text-white">
+        <Link href="/">Tasty Next</Link>
+      </h1>
+    </nav>
+  );
+}
